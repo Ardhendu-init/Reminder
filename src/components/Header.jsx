5ea7b205a5737cf3c6c9d6d3a58ab22f5663022d
Ardhendu <ardhendujj002@gmail.com>
2021-07-05 12:19:58 +0530

    Reminder app ist commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
+function Header() {
+  return (
+    <header>
+      <h1>
+        {" "}
+        <NotificationsActiveIcon /> Reminder
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
